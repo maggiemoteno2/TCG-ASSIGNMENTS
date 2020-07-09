ff06e15d01f7e4f9d1d4e8895b99f5ce210273a6
maggiemoteno2 <motenomaggie@gmail.com>
2020-07-09 16:29:48 +0200

    added an action

--- rsvp-app/src/redux/detailsForm/actions.js
+++ rsvp-app/src/redux/detailsForm/actions.js
@@ -1,1 +1,20 @@
 import {ADD_DETAILS} from './../actionTypes'
+import axios from "axios"
+
+export const postApplicantInfo=(firstname,email) =>{
+    return async dispatch => {
+        try {
+          console.log("sent data", { firstname, email });
+          const { data } = await axios.post("http://localhost:3004/applicantInformation", {
+            firstname,
+            email
+          });
+          dispatch({
+            type: ADD_DETAILS,
+            payload: { ...data }
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      };
+}
